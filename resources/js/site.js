d2ffcc6b21bad820399789aaeb4111862f35b7d1
OctoY <yann@octoplus.solutions>
2026-06-04 12:18:04 +0200

    Add Lenis smooth scrolling library to enhance user experience

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,6 +1,16 @@
 import Alpine from 'alpinejs';
+import Lenis from 'lenis';
 
 window.Alpine = Alpine;
+
+const lenis = new Lenis();
+window.lenis = lenis;
+
+function raf(time) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
 document.addEventListener('alpine:init', () => {
 	Alpine.data('historyCarousel', () => ({
