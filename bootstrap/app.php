be5f07b6c3a8e8287222c69a62810e2d0067dc46
OctoY <yann@octoplus.solutions>
2026-06-18 13:32:35 +0200

    pint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -15,11 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         if ($proxies) {
             $middleware->trustProxies(
                 at: $proxies === '*' ? '*' : explode(',', $proxies),
-                headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR
-                    | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST
-                    | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT
-                    | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
-                    | \Illuminate\Http\Request::HEADER_X_FORWARDED_PREFIX,
+                headers: Request::HEADER_X_FORWARDED_FOR
+                    | Request::HEADER_X_FORWARDED_HOST
+                    | Request::HEADER_X_FORWARDED_PORT
+                    | Request::HEADER_X_FORWARDED_PROTO
+                    | Request::HEADER_X_FORWARDED_PREFIX,
             );
         }
     })
